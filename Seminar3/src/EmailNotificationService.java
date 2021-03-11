b281968455b7b91bcfd0e393d3bc8a60f8940b7a
Mirela <Mirela@10.38.36.2>
2021-03-11 05:21:23 -0800

    afiseaza corect si emailul

--- Seminar3/src/EmailNotificationService.java
+++ Seminar3/src/EmailNotificationService.java
@@ -3,7 +3,7 @@ public class EmailNotificationService implements NotificationService{
 
 	@Override
 	public void sendNotificationService(Person destination, String msg) {
-		System.out.println("Sending email to " + destination);
+		System.out.println("Sending email to " + destination.getEmail());
 		System.out.println(msg);
 	}
 
