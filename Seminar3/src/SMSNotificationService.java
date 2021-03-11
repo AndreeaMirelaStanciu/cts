6b798553a8e8edd8580637c0c28a1115941ff5cc
Mirela <Mirela@10.38.36.2>
2021-03-11 05:17:49 -0800

    afiseaza corect numarul de telefon la consola

--- Seminar3/src/SMSNotificationService.java
+++ Seminar3/src/SMSNotificationService.java
@@ -3,7 +3,7 @@ public class SMSNotificationService implements NotificationService {
 
 	@Override
 	public void sendNotificationService(Person destination, String msg) {
-		System.out.println("Sending SMS to " + destination);
+		System.out.println("Sending SMS to " + destination.getMobile());
 		System.out.println(msg);
 	}
 
