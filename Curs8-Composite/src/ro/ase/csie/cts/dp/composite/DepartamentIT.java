40c55e68b44df464fd79cf417eae1893b440436a
Mirela <Mirela@DESKTOP-BNJ47IP>
2021-04-27 07:24:49 +0300

    Implementare Composite

---
+++ Curs8-Composite/src/ro/ase/csie/cts/dp/composite/DepartamentIT.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.dp.composite;
+
+public class DepartamentIT {
+	// referinta catre nodul radacina
+	NodStructuraIerarhica structura;
+
+	public DepartamentIT(StructuraOrganizatorica departament) {
+		this.structura = departament;
+	}
+}
