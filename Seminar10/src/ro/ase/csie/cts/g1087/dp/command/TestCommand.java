1811900b94b7a151881a6d99364401593d8a0297
Mirela <Mirela@DESKTOP-BNJ47IP>
2021-05-12 14:24:38 +0300

    Adaugare ManagerTaskuri la Command

--- Seminar10/src/ro/ase/csie/cts/g1087/dp/command/TestCommand.java
+++ Seminar10/src/ro/ase/csie/cts/g1087/dp/command/TestCommand.java
@@ -2,11 +2,21 @@ package ro.ase.csie.cts.g1087.dp.command;
 
 public class TestCommand {
 	public static void main(String[] args) {
+		
+		ManagerTaskuri manager = new ManagerTaskuri();
+		
 		TaskAsincron autoSave = new TaskToDo(new ModulAutoSave(), "Autosave", 5);
 		TaskAsincron autoUpdate = new TaskToDo(new ModulUpdateClient(), "Update v2", 20);
 		
 		//executie task-uri asincrone
-		autoSave.executaTask();
-		autoUpdate.executaTask();
+		//autoSave.executaTask();
+		//autoUpdate.executaTask();
+		
+		System.out.println("Jocul este activ");
+		
+		manager.adaugaTask(autoSave);
+		manager.adaugaTask(autoUpdate);
+		
+		System.out.println("Jocul este activ");
 	}
 }
