08737cae2efba28160b388c2860e00d39ad2d918
Mirela <Mirela@DESKTOP-BNJ47IP>
2021-05-13 13:07:54 +0300

    Implementare Strategy

---
+++ Curs9-Strategy/src/ro/ase/csie/cts/dp/creationale/strategy/EntitateMedie.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.dp.creationale.strategy;
+
+public class EntitateMedie implements IProcesabil{
+	@Override
+	public int proceseaza(int[] valori) {
+		if(valori == null)
+			return 0;
+		int medie = 0;
+		for(int i=0;i<valori.length;i++)
+			medie+=valori[i];
+		return medie/valori.length;
+	}
+}
---
+++ Curs9-Strategy/src/ro/ase/csie/cts/dp/creationale/strategy/IProcesabil.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.dp.creationale.strategy;
+
+public interface IProcesabil {
+	public abstract int proceseaza(int valori[]);
+}
